a37135d607d7c15b560669bfadb2a774aabd30d4
ddanelynn <dingyoujia0823@gmail.com>
2022-09-20 23:16:01 +0800

    check for primitive skills

--- Parser.py
+++ Parser.py
@@ -2,23 +2,37 @@ import json
 from Task import *
 from Member import *
 
+primitive_skills = set()
+data = None
+
 def readTestCase(filename):
     file = open(filename)
     data = json.load(file)
-    parseMembers(data['members'])
-    parseTasks(data['tasks'])
+    members = parseMembers(data['members'])
+    tasks = parseTasks(data['tasks'])
+    return (members, tasks)
 
 def parseTasks(tasks):
+    tasks = []
     for task in tasks:
         name = task['name']
         prerequisites = task['prerequisite']
         skillsRequired = task['skills_required']
-        newTask = Task(name, prerequisites, skillsRequired)
+        for skill in skillsRequired:
+            if skill not in primitive_skills:
+                skillsRequired.extend(data[skill])
+        tasks.append(Task(name, prerequisites, skillsRequired))
+    return tasks
 
 
 def parseMembers(members):
+    members = []
     for member in members:
         name = member['name']
         skills = member['skills']
-        newMember = Member(name, skills)
+        for skill in skills:
+            primitive_skills.add(skill)
+        members.append(Member(name, skills))
+    return members
+
     
